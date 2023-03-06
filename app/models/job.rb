0383df897d4d196ed335cc9925fb35f031b3f149
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-03-06 16:05:38 -0300

    added geocode instruction to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,6 @@ class Job < ApplicationRecord
   has_many :my_jobs
   validates :name, :description, :category, :address, :city, :qty_hour, presence: true
   validates :price_per_hour, :start_date, :end_date, :start_time, :end_time, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end

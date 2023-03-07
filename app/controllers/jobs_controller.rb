8ed008b37385a236d016117f8eb2d3831e76918b
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-03-07 16:21:01 -0300

    added map to index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,10 +7,18 @@ class JobsController < ApplicationController
       @jobs = Job.search_by_name_and_description_and_city(params[:query])
     end
 
+    @markers = @jobs.geocoded.map do |job|
+      {
+        lat: job.latitude,
+        lng: job.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { job: job }, formats: [:html])
+      }
+    end
+
     respond_to do |format|
       format.html
       format.text { render partial: "jobs/list", locals: { jobs: @jobs }, formats: [:html] }
     end
-  end
 
+  end
 end

6a1fbaf1c5f40645bba8ba7ec7e6ca7fa4272587
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-03-07 12:39:29 -0300

    completed javascript function of search in index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,16 @@
 class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+
+    if params[:query].present?
+      @jobs = @jobs.where("name ILIKE ?", "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "jobs/list", locals: { jobs: @jobs }, formats: [:html] }
+    end
+  end
+
 end

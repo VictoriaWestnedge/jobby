9c596ff56649504ff31bd797e6d2ed272fefcd71
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-03-10 15:26:41 -0300

    changed description in button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ if Rails.env.development?
   Review.destroy_all
   MyJob.destroy_all
   Job.destroy_all
+  Message.destroy_all
   User.destroy_all
   puts "Data deleted successfully"
  end
@@ -28,19 +29,83 @@ user3.photo.attach(io: file, filename: "fdo.png", content_type: "image/png")
 user3.save
 puts "#{user3.nickname} created successfully"
 
-job1 = Job.create!(name: "Painting my first floor", description: "I need help with redecorating my first floor, sitting room and kitchen", category: 1, address: "Av. La Florida 3567", city: "Santiago", qty_hour: 5, price_per_hour: 15, start_date: Date.new(2023,4,1), end_date: Date.new(2023,4,1), start_time: Time.now.strftime("10:00:00"), end_time: Time.now.strftime("15:00:00"), user: user1)
+user4 = User.create!(nickname: "Andres", email: "andres@gmail.com", password: "123456")
+file = URI.open("https://avatars.githubusercontent.com/u/47392684?v=4")
+user4.photo.attach(io: file, filename: "andres.png", content_type: "image/png")
+user4.save
+puts "#{user4.nickname} created successfully"
 
-# file = URI.open("https://pinturashipopotamo.es/wp-content/uploads/2020/01/como-elegir-un-buen-pintor.jpg")
-# job1.photo.attach(io: file, filename: "r1.png", content_type: "image/png")
+user5 = User.create!(nickname: "Cecilia", email: "cecilia@gmail.com", password: "123456")
+file = URI.open("https://avatars.githubusercontent.com/u/73177352?v=4")
+user5.photo.attach(io: file, filename: "ceci.png", content_type: "image/png")
+user5.save
+puts "#{user5.nickname} created successfully"
 
-job2 = Job.create!(name: "Babysitter needed", description: "I am looking for a babysitter to look after my 4 year old girl", category: 2, address: "Av. Bernardo O'higgins 1267", city: "Santiago", qty_hour: 4, price_per_hour: 30, start_date: Date.new(2023,3,15), end_date: Date.new(2023,3,15), start_time: Time.now.strftime("19:00:00"), end_time: Time.now.strftime("23:00:00"), user: user1)
+job1 = Job.create!(name: "Painting my first floor", description: "I need help with redecorating my first floor, sitting room and kitchen", category: "Painting", address: "Av. La Florida 3567", city: "Santiago", qty_hour: 5, price_per_hour: 15, start_date: Date.new(2023,4,1), end_date: Date.new(2023,4,1), start_time: Time.now.strftime("10:00:00"), end_time: Time.now.strftime("15:00:00"), user: user1)
+file = URI.open("https://pinturashipopotamo.es/wp-content/uploads/2020/01/como-elegir-un-buen-pintor.jpg")
+job1.photo.attach(io: file, filename: "j1.png", content_type: "image/png")
 
-# file = URI.open("https://heritagevalleyfcu.org/wp-content/uploads/2019/09/shutterstock_552072628-e1595863177777.jpg")
-# job2.photo.attach(io: file, filename: "r2.png", content_type: "image/png")
+job2 = Job.create!(name: "Babysitter needed", description: "I am looking for a babysitter to look after my 4 year old girl", category: "Babysitting", address: "Blest Gana 5888", city: "Santiago", qty_hour: 4, price_per_hour: 30, start_date: Date.new(2023,3,15), end_date: Date.new(2023,3,15), start_time: Time.now.strftime("19:00:00"), end_time: Time.now.strftime("23:00:00"), user: user1)
+file = URI.open("https://heritagevalleyfcu.org/wp-content/uploads/2019/09/shutterstock_552072628-e1595863177777.jpg")
+job2.photo.attach(io: file, filename: "j2.png", content_type: "image/png")
 
-job3 = Job.create!(name: "Delivery of packages", description: "Looking for someone that can deliver some heavy boxes to my office", category: 3, address: "Av. Recoleta 3500", city: "Valparaiso", qty_hour: 2, price_per_hour: 20, start_date: Date.new(2023,3,20), end_date: Date.new(2023,3,20), start_time: Time.now.strftime("16:00:00"), end_time: Time.now.strftime("18:00:00"), user: user2)
-# file = URI.open("https://freedesignfile.com/upload/2018/10/Smiling-young-delivery-guy-Stock-Photo-04.jpg")
-# job3.photo.attach(io: file, filename: "r3.png", content_type: "image/png")
+job3 = Job.create!(name: "Delivery of packages", description: "Looking for someone that can deliver some heavy boxes to my office", category: "Deliveries", address: "Av. Los Pajaritos 3271", city: "Santiago", qty_hour: 2, price_per_hour: 20, start_date: Date.new(2023,3,20), end_date: Date.new(2023,3,20), start_time: Time.now.strftime("16:00:00"), end_time: Time.now.strftime("18:00:00"), user: user3)
+file = URI.open("https://freedesignfile.com/upload/2018/10/Smiling-young-delivery-guy-Stock-Photo-04.jpg")
+job3.photo.attach(io: file, filename: "j3.png", content_type: "image/png")
+
+job4 = Job.create!(name: "Petsitting my dog", description: "Looking for someone to look after my dog Leia for the weekend", category: "Petsitting", address: "Av. Jorge Montt 11700", city: "Talcahuano", qty_hour: 12, price_per_hour: 10, start_date: Date.new(2023,3,18), end_date: Date.new(2023,3,19), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user3)
+file = URI.open("https://bloximages.chicago2.vip.townnews.com/eacourier.com/content/tncms/assets/v3/editorial/5/11/51146d02-bc2f-11ed-a460-d7bbde0df6e7/6406000e42d69.image.jpg")
+job4.photo.attach(io: file, filename: "j4.png", content_type: "image/png")
+
+job5 = Job.create!(name: "Cleaning house", description: "Help to organize and clean my house after moving in", category: "Cleaning", address: "Av. La Florida 3567", city: "Santiago", qty_hour: 6, price_per_hour: 15, start_date: Date.new(2023,3,25), end_date: Date.new(2023,3,25), start_time: Time.now.strftime("09:00:00"), end_time: Time.now.strftime("15:00:00"), user: user4)
+file = URI.open("https://www.familyhandyman.com/wp-content/uploads/2022/08/GettyImages-562452123.jpg")
+job5.photo.attach(io: file, filename: "j5.png", content_type: "image/png")
+
+job6 = Job.create!(name: "Pick up dog food", description: "I need someone to pick-up my dog food from the city centre", category: "Deliveries", address: "Av. Los Pajaritos 3271", city: "Santiago", qty_hour: 2, price_per_hour: 8, start_date: Date.new(2023,4,1), end_date: Date.new(2023,4,1), start_time: Time.now.strftime("09:00:00"), end_time: Time.now.strftime("10:00:00"), user: user4)
+file = URI.open("https://www.canadianpetconnection.ca/wp-content/uploads/2022/01/Oven-Baked-Tradition-Small-Breed-All-Staged-Food-22LB-.png")
+job6.photo.attach(io: file, filename: "j6.png", content_type: "image/png")
+
+job7 = Job.create!(name: "Furniture assembly", description: "Please help assemble a cot for my niece", category: "Others", address: "Av. Los Pajaritos 3271", city: "Santiago", qty_hour: 3, price_per_hour: 12, start_date: Date.new(2023,4,10), end_date: Date.new(2023,4,10), start_time: Time.now.strftime("11:00:00"), end_time: Time.now.strftime("13:00:00"), user: user4)
+file = URI.open("https://www.roomtogrow.co.uk/product_images/uploaded_images/furniture.jpg")
+job7.photo.attach(io: file, filename: "j7.png", content_type: "image/png")
+
+job8 = Job.create!(name: "Babysitting my son", description: "Looking for someone to look after my baby Alexander", category: "Babysitting", address: "Ramón Cruz Montt 2895", city: "Santiago", qty_hour: 12, price_per_hour: 10, start_date: Date.new(2023,3,18), end_date: Date.new(2023,3,19), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user4)
+file = URI.open("https://ichef.bbci.co.uk/news/976/cpsprodpb/11B4A/production/_128622527_cairo.jpg")
+job8.photo.attach(io: file, filename: "j8.png", content_type: "image/png")
+
+job9 = Job.create!(name: "Look after my dog", description: "Looking for someone to look after my dog Princessa", category: "Petsitting", address: "Av. Los Pajaritos 3271", city: "Santiago", qty_hour: 12, price_per_hour: 10, start_date: Date.new(2023,3,18), end_date: Date.new(2023,3,19), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user5)
+file = URI.open("https://paradepets.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkxMzY1Nzg4NjczMzIwNTQ2/cutest-dog-breeds-jpg.jpg")
+job9.photo.attach(io: file, filename: "j9.png", content_type: "image/png")
+
+job10 = Job.create!(name: "Organizing my garage", description: "I have been doing a sort out and need some help tidying my garage", category: "Cleaning", address: "Nuncio Laghi 6724", city: "Santiago", qty_hour: 12, price_per_hour: 10, start_date: Date.new(2023,3,18), end_date: Date.new(2023,3,19), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user5)
+file = URI.open("https://thecleanhaven.com/wp-content/uploads/2019/09/clean-house.jpeg")
+job10.photo.attach(io: file, filename: "j10.png", content_type: "image/png")
+
+job11 = Job.create!(name: "Deliveries to warehouse", description: "Need someone with a car to deliver some boxes to my warehouse", category: "Deliveries", address: "Ramón Cruz Montt 2895", city: "Santiago", qty_hour: 12, price_per_hour: 10, start_date: Date.new(2023,3,18), end_date: Date.new(2023,3,19), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user5)
+file = URI.open("https://www.vanvault.co.uk/media/catalog/product/cache/1/overlay_image/285x/9df78eab33525d08d6e5fb8d27136e95/v/a/van_vault_stacker_xl-app.jpg")
+job11.photo.attach(io: file, filename: "j11.png", content_type: "image/png")
+
+job12 = Job.create!(name: "Cat sitting overnight", description: "I am going away for the weekend and need someone to look after my cats", category: "Petsitting", address: "Nuncio Laghi 6724", city: "Santiago", qty_hour: 12, price_per_hour: 5, start_date: Date.new(2023,3,25), end_date: Date.new(2023,3,26), start_time: Time.now.strftime("20:00:00"), end_time: Time.now.strftime("08:00:00"), user: user5)
+file = URI.open("https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg")
+job12.photo.attach(io: file, filename: "j12.png", content_type: "image/png")
+
+puts "Jobs created successfully"
+
+review1 = Review.create!(rating_star: 5, description: "Excellent job, really happy with her work", user: user2)
+
+review2 = Review.create!(rating_star: 4, description: "Very happy with Nica's work, great painting", user: user2)
+
+review3 = Review.create!(rating_star: 4, description: "So pleased with how everything turned out", user: user2)
+
+review4 = Review.create!(rating_star: 3, description: "Ok work, but she seemed in a bit of a rush", user: user2)
+
+review5 = Review.create!(rating_star: 2, description: "She arrived late and didn't finish everything", user: user2)
+
+review6 = Review.create!(rating_star: 4, description: "Great work, would love to work with her again!", user: user2)
+
+review7 = Review.create!(rating_star: 5, description: "Can't be happier, she did an amazing job painting", user: user2)
+
+puts "reviews created successfully"
 
 puts "Creating Chatroom"
   Chatroom.create(name: "general")

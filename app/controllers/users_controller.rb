584f9d9677c116b2ab86029d46e830f1c912e77b
fernando ramirez <patricio_129@hotmail.com>
2023-03-10 19:24:52 -0600

    Agregue el rating en my jobs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_reviews = @user.reviews.order(created_at: :desc)
     @review = Review.new
+    rating = 0
+    @user_reviews.each {|user| rating += user.rating_star}
+    @comon = rating/@user_reviews.size
+    @user.rating = @comon
+    @user.save
     # @date = Date.today.strftime('%d/%m/%Y')
   end
 

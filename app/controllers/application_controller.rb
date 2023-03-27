21fa5e81cdfb90640a7383e80cde928eee68c624
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-03-27 12:24:58 -0300

    updates to signup edit and new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,15 @@ class ApplicationController < ActionController::Base
     I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
     redirect_to root_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :photo])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :photo])
+  end
+
 end
